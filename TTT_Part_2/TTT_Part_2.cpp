907eccc7bf3c05a983916473f8d660cb55e3402a
LeoG123321 <3545gonzalez@gmail.com>
2024-05-09 17:32:03 -0500

    updated int main(), however program failed to start

--- TTT_Part_2/TTT_Part_2.cpp
+++ TTT_Part_2/TTT_Part_2.cpp
@@ -1,6 +1,6 @@
-//#include "Gameplay.h"  
-//#include "computer_player.h"
-//#include "human_player.h"
+#include "Gameplay.h"  
+#include "computer_player.h"
+#include "human_player.h"
 #include <iomanip>
 #include <iostream>
 #include "Board.h"
@@ -9,7 +9,6 @@ using namespace std;
 
 int main()
 {
-	/*
 	int main_menu_input;
 
 	Gameplay* game_ptr;
@@ -24,23 +23,41 @@ int main()
 main_menu_input_retry:
 	cin >> main_menu_input;
 
-	if (main_menu_input == 1) {
-		human_player player_1('X');
-		player* player_1_ptr = &player_1;
-		human_player player_2('O');
-		player* player_2_ptr = &player_2;
-		Gameplay game(player_1_ptr, player_2_ptr);
-		game_ptr = &game;
-		game.gameRun();
-	}
-	else if (main_menu_input == 2) {
-		human_player player_1('X');
-		player* player_1_ptr = &player_1;
-		computer_player player_2('O');
-		player* player_2_ptr = &player_2;
-		Gameplay game(player_1_ptr, player_2_ptr);
-		game_ptr = &game;
-		game.gameRun();
+	if (main_menu_input == 1 || main_menu_input == 2) {
+		int size_input;
+		cout << endl << endl;
+
+	size_try_again:
+		cout << "Please Enter the Size of the Board (N,N):" << endl
+			<< "Example Input: 3" << endl
+			<< "Result: Size = 3x3 Board" << endl
+			<< "Input Range > 2" << endl;
+
+		cin >> size_input;
+		if (size_input > 2) {
+			if (main_menu_input == 1) {
+				human_player player_1('X');
+				player* player_1_ptr = &player_1;
+				human_player player_2('O');
+				player* player_2_ptr = &player_2;
+				Gameplay game(player_1_ptr, player_2_ptr, size_input);
+				game_ptr = &game;
+				game.gameRun();
+			}
+			else if (main_menu_input == 2) {
+				human_player player_1('X');
+				player* player_1_ptr = &player_1;
+				computer_player player_2('O');
+				player* player_2_ptr = &player_2;
+				Gameplay game(player_1_ptr, player_2_ptr, size_input);
+				game_ptr = &game;
+				game.gameRun();
+			}
+		}
+		else {
+			cout << "Error: Input Invalid. Try Again:" << endl;
+			goto size_try_again;
+		}
 	}
 	else {
 		cout << "Error: Input Invalid. Try Again:" << endl;
@@ -65,31 +82,6 @@ main_menu_input_retry:
 			cout << "Goodbye! Have a nice day! :)" << endl;
 		}
 	} while (play_again_input == 'y');
-	*/
 
-	// Testing Board
-	Board board(3);
-	board.setCell(2, 2, 'X');
-	board.setCell(0, 0, 'X');
-	board.setCell(1, 0, 'O');
-	board.setCell(1, 2, 'W');
-	board.displayBoard();
-
-	cout << "At 2,2 = " << board.getCell(2, 2) << endl;
-	cout << "At 0,0 = " << board.getCell(0, 0) << endl;
-	cout << "At 1,0 = " << board.getCell(1, 0) << endl;
-	cout << "At 1,2 = " << board.getCell(1, 2) << endl;
-
-
-	/*
-	Board board2(5);
-	board2.displayBoard();
-
-	
-	Board board3(10);
-	board3.setCell(9, 9, 'O');
-	board3.setCell(0, 0, 'X');
-	board3.displayBoard();
-	*/
 		return 0;
 }
